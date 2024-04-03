7c4c83d47bde755e612b59d75ebd745ce69a42a1
Izzo2le <isobel.otoole@aim.com>
2024-04-03 17:41:38 +0000

    Added Gunicorn, updated Procfile and settings for Heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(n&9$f$sz256r2d9@md*ljs=d!1msjo!9c_nu^if=g&c=@wcga'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['app.herokuapp.com']
 
 
 # Application definition
