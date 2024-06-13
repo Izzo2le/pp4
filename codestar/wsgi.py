78860df1d50e7d6f6849e9725522eb0ebe883586
Izzo2le <isobel.otoole@aim.com>
2024-06-13 09:31:53 +0000

    wsgi.py update

--- codestar/wsgi.py
+++ codestar/wsgi.py
@@ -1,17 +1,8 @@
-"""
-WSGI config for codestar project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-load_dotenv() 
+load_dotenv()  # This loads the .env file
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
 
