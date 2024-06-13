8afcc3048575712580f1f6100516836a5b037de1
Izzo2le <isobel.otoole@aim.com>
2024-06-13 09:29:49 +0000

    manage.py update

--- manage.py
+++ manage.py
@@ -4,16 +4,12 @@ import os
 import sys
 from dotenv import load_dotenv
 
-
 def main():
-     load_dotenv()
-     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
-
-
     """Run administrative tasks."""
+    load_dotenv()  # This loads the .env file
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar.settings')
     try:
-    from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
